62fd135c3a98b6aa1ee6411fa16dd198c7714e97
Nishant Upadhyay <nishant.upadhyay@beaverlogic.com>
2015-04-10 13:04:23 +0530

    add -

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   
   def welcome
     current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
+    puts '----------------------------'
     @callback_url = "http://#{current_host}/login"
   end
   
